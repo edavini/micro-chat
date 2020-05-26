ac253eeabfac5ac91348149d90ee1058c95f62bc
Edoardo Davini <edoardo.davini@gmail.com>
2020-05-27 00:12:09 +0200

    sudo wasn't the way

--- src/environments/decrypt.sh
+++ src/environments/decrypt.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # --batch to prevent interactive command
-gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output environment.prod.ts environment.prod.encrypted.ts.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output environment.prod.ts environment.prod.encrypted.ts.gpg
